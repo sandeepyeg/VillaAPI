b1381934b5cfd796776d50a760877458405d013f
yegsandeep <yegsandeep@gmail.com>
2024-01-04 00:05:08 -0700

    asas

---
+++ VillaAPI/Controllers/VillaController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VillaAPI.Controllers
+{
+	public class VillaController:ControllerBase
+	{
+		public VillaController()
+		{
+		}
+	}
+}
+
